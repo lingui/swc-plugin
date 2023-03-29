f0c289ae5b9810cecea959bc1de153ac247f7a13
Timofei Iatsenko <ty@time.sc>
2023-03-29 17:09:36 +0200

    showcase different methods of initializing nextjs app with lingui

--- examples/nextjs-13/src/pages/_app.tsx
+++ examples/nextjs-13/src/pages/_app.tsx
@@ -1,10 +1,54 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { I18nProvider } from '@lingui/react';
-import { useLinguiInit } from '../i18n';
+import { i18n } from '@lingui/core';
+// import { useEffect, useRef } from 'react';
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
-  const i18n = useLinguiInit(pageProps.i18n);
+  const locale = router.locale || router.defaultLocale!
+  const messages = pageProps.i18n;
+
+  // attempt 1
+  // open in browser and try to change the language
+  // Warning: Cannot update a component (`I18nProvider`) while rendering a different component (`MyApp`). To locate the bad setState() call inside `MyApp`
+  // ====
+  i18n.load(locale, messages);
+  i18n.activate(locale);
+
+  // attempt 2
+  // this would simply don't work in SSR, because of useEffect
+  // ====
+  // useEffect(() => {
+  //   i18n.load(locale, messages);
+  //   i18n.activate(locale);
+  // }, [locale, pageProps.i18n])
+  // ====
+
+  // attempt 3
+  // this would work both in SSR and no issue in runtime but very verbose
+  // ====
+  // const firstRender = useRef(true)
+  //
+  // // run only once on the first render (for server side)
+  // if (messages && firstRender.current) {
+  //   i18n.load(locale, messages)
+  //   i18n.activate(locale)
+  //   firstRender.current = false
+  // }
+  //
+  // useEffect(() => {
+  //   if (messages) {
+  //     i18n.load(locale, messages)
+  //     i18n.activate(locale)
+  //   }
+  // }, [locale, messages])
+  // ====
+
+  // attempt 4 (current)
+  // just set data without calling an event
+  // ====
+  // i18n.loadAndActivate(locale, messages, false);
+  // ====
 
   return (
     <I18nProvider i18n={i18n}>
